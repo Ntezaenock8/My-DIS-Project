71559eeec6b72bb85f819919ad35324fb806628a
software33 <softwarerecess09@gmail.com>
2022-01-26 14:34:58 +0300

    Registration for the teachers, log in and sign up

---
+++ kinderlogform.php
@@ -0,0 +1,22 @@
+﻿<?php
+	session_start();
+	if(isset($_POST['login'])){
+		$email = $_POST['email'];
+		$password = $_POST['pass'];
+
+		require_once 'kinderconnect.php';
+		require_once 'kinderfunctions.php';
+
+		if(emptyLogin($email, $password) !== false){
+			header("location: kinderlogin.php?error=emptyinput");
+			exit();
+		}
+
+		if(loginuser($conn, $email, $password)==false){
+			header("location: kinderlogin.php?error=loginfail");
+			exit();
+		}
+		else{
+			header("location: kinderlogin.php?error=none/login succesful");
+		}
+	}
