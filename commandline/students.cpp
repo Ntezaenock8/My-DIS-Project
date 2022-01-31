4e3231163998d97d28b4d891dae1ccf7988e6c6f
wksn753 <98258939+wksn753@users.noreply.github.com>
2022-01-31 15:30:03 +0300

    Add files via upload
    this module helps student attempt test set by teacher

--- commandline/students.cpp
+++ commandline/students.cpp
@@ -1,5 +1,6 @@
 #include<stdio.h>
-#include<conio.h>
+#include<time.h>
+#include<mysql.h>
 char ltr[7][4],test_c;
 char ans[7][4]={
                 {' ',' ',' ',' '},
@@ -10,9 +11,11 @@ char ans[7][4]={
                 {' ',' ',' ',' '},
                 {' ',' ',' ',' '},
 };
+clock_t start,stop;
 char mark='*';
 int marks,i,j,choice;
 char letter;
+char trs[4]={' ',' ',' ',' '};
 
 void drawboard(){
 	printf("   %c|   %c|   %c|   %c\n",ans[0][0],ans[0][1],ans[0][2],ans[0][3]);
@@ -28,15 +31,19 @@ void drawboard(){
 		printf("   %c|   %c|   %c|   %c\n",ans[5][0],ans[5][1],ans[5][2],ans[5][3]);
 	printf("____|____|____|____\n");
 	printf("   %c|   %c|   %c|   %c\n",ans[6][0],ans[6][1],ans[6][2],ans[6][3]);
-	
+
 }
-void attempt_test(){
-	char test[4]={'F','E','H','L'};
+void attempt_test(char *ptr){
+         for(i=0;i<strlen(ptr);i++){
+         trs[i]=ptr[i];
+         printf("%c\n",trs[i]);
+ }
 for(int k=0;k<4;k++){
-	    letter=test[k];
+        letter=trs[k];
 	    i=0;
-	    marks=0; 
-	    printf("draw pattern for letter %c\n",test[k]);
+	    marks=0;
+	    printf("draw pattern for letter %c\n",letter);
+	    start=clock();
 		do{
 
 		for(j=0;j<4;j++){
@@ -62,7 +69,7 @@ for(int k=0;k<4;k++){
 	           {'*',' ',' ',' '},
 	           {'*',' ',' ',' '},
 	           {'*',' ',' ',' '},
-         
+
      };
 
 	for(i=0;i<7;i++){
@@ -82,7 +89,7 @@ for(int k=0;k<4;k++){
 	           {'*',' ',' ','*'},
 	           {'*',' ',' ','*'},
 	           {'*','*','*',' '},
-         
+
      };
 
 	for(i=0;i<7;i++){
@@ -102,7 +109,7 @@ for(int k=0;k<4;k++){
 	           {'*',' ',' ',' '},
 	           {'*',' ',' ',' '},
 	           {' ','*','*','*'},
-         
+
      };
 
 	for(i=0;i<7;i++){
@@ -122,7 +129,7 @@ for(int k=0;k<4;k++){
 	           {'*',' ',' ','*'},
 	           {'*',' ',' ','*'},
 	           {'*',' ',' ','*'},
-         
+
      };
 
 	for(i=0;i<7;i++){
@@ -142,7 +149,7 @@ for(int k=0;k<4;k++){
 	           {'*',' ',' ','*'},
 	           {'*',' ',' ','*'},
 	           {'*','*','*',' '},
-         
+
      };
 
 	for(i=0;i<7;i++){
@@ -162,7 +169,7 @@ for(int k=0;k<4;k++){
 	           {'*',' ',' ',' '},
 	           {'*',' ',' ',' '},
 	           {'*','*','*','*'},
-         
+
      };
 
 	for(i=0;i<7;i++){
@@ -182,7 +189,7 @@ for(int k=0;k<4;k++){
 	           {'*',' ',' ','*'},
 	           {'*',' ',' ','*'},
 	           {'*','*','*','*'},
-         
+
      };
 
 	for(i=0;i<7;i++){
@@ -202,7 +209,7 @@ for(int k=0;k<4;k++){
 	           {'*',' ',' ','*'},
 	           {'*',' ',' ','*'},
 	           {'*',' ',' ','*'},
-         
+
      };
 
 	for(i=0;i<7;i++){
@@ -222,7 +229,7 @@ for(int k=0;k<4;k++){
 	           {' ','*','*',' '},
 	           {'*','*','*','*'},
 	           {'*','*','*','*'},
-         
+
      };
 
 	for(i=0;i<7;i++){
@@ -242,7 +249,7 @@ for(int k=0;k<4;k++){
 	           {' ','*','*',' '},
 	           {'*','*','*',' '},
 	           {'*','*','*',' '},
-         
+
      };
 
 	for(i=0;i<7;i++){
@@ -262,7 +269,7 @@ for(int k=0;k<4;k++){
 	           {' ','*','*',' '},
 	           {' ','*','*',' '},
 	           {' ','*','*',' '},
-         
+
      };
 
 	for(i=0;i<7;i++){
@@ -282,9 +289,9 @@ for(int k=0;k<4;k++){
 	           {'*',' ','*',' '},
 	           {'*',' ',' ','*'},
 	           {'*',' ',' ','*'},
-         
+
      };
-     
+
 
 	for(i=0;i<7;i++){
 		for(j=0;j<4;j++){
@@ -303,7 +310,7 @@ for(int k=0;k<4;k++){
 	           {'*',' ',' ','*'},
 	           {'*',' ',' ','*'},
 	           {'*',' ',' ','*'},
-         
+
      };
 
 	for(i=0;i<7;i++){
@@ -323,7 +330,7 @@ for(int k=0;k<4;k++){
 	           {' ','*','*',' '},
 	           {' ','*','*',' '},
 	           {' ','*','*',' '},
-         
+
      };
 
 	for(i=0;i<7;i++){
@@ -343,7 +350,7 @@ for(int k=0;k<4;k++){
 	           {'*',' ',' ','*'},
 	           {'*',' ',' ','*'},
 	           {' ','*','*',' '},
-         
+
      };
 
 	for(i=0;i<7;i++){
@@ -363,7 +370,7 @@ for(int k=0;k<4;k++){
 	           {'*',' ',' ','*'},
 	           {'*',' ',' ','*'},
 	           {'*','*','*','*'},
-         
+
      };
 
 	for(i=0;i<7;i++){
@@ -383,7 +390,7 @@ for(int k=0;k<4;k++){
 	           {'*',' ',' ',' '},
 	           {'*',' ',' ',' '},
 	           {'*','*','*','*'},
-         
+
      };
 
 	for(i=0;i<7;i++){
@@ -403,7 +410,7 @@ for(int k=0;k<4;k++){
 	           {'*',' ',' ','*'},
 	           {'*',' ',' ','*'},
 	           {'*',' ',' ','*'},
-         
+
      };
 
 	for(i=0;i<7;i++){
@@ -423,7 +430,7 @@ for(int k=0;k<4;k++){
 	           {'*',' ','*','*'},
 	           {'*',' ',' ','*'},
 	           {'*',' ',' ','*'},
-         
+
      };
 
 	for(i=0;i<7;i++){
@@ -443,7 +450,7 @@ for(int k=0;k<4;k++){
 	           {'*',' ',' ','*'},
 	           {'*',' ',' ','*'},
 	           {' ','*','*',' '},
-         
+
      };
 
 	for(i=0;i<7;i++){
@@ -463,7 +470,7 @@ for(int k=0;k<4;k++){
 	           {'*',' ',' ',' '},
 	           {'*',' ',' ',' '},
 	           {'*',' ',' ',' '},
-         
+
      };
 
 	for(i=0;i<7;i++){
@@ -483,7 +490,7 @@ for(int k=0;k<4;k++){
 	           {'*',' ',' ','*'},
 	           {'*',' ',' ','*'},
 	           {'*',' ',' ','*'},
-         
+
      };
 
 	for(i=0;i<7;i++){
@@ -503,7 +510,7 @@ for(int k=0;k<4;k++){
 	           {'*','*',' ','*'},
 	           {' ','*','*',' '},
 	           {' ',' ',' ','*'},
-         
+
      };
 
 	for(i=0;i<7;i++){
@@ -523,7 +530,7 @@ for(int k=0;k<4;k++){
 	           {' ',' ',' ','*'},
 	           {' ',' ',' ','*'},
 	           {'*','*','*',' '},
-         
+
      };
 
 	for(i=0;i<7;i++){
@@ -543,7 +550,7 @@ for(int k=0;k<4;k++){
 	           {'*',' ',' ','*'},
 	           {'*','*','*','*'},
 	           {'*',' ',' ','*'},
-         
+
      };
 
 	for(i=0;i<7;i++){
@@ -563,7 +570,7 @@ for(int k=0;k<4;k++){
 	           {' ',' ',' ',' '},
 	           {'*',' ',' ',' '},
 	           {'*','*','*','*'},
-         
+
      };
 
 	for(i=0;i<7;i++){
@@ -575,7 +582,9 @@ for(int k=0;k<4;k++){
 	}
 	}
 
-
+    stop=clock();
+    double time_taken=(double)(stop-start)/1000;
+    printf("\n\nit took %f seconds\n",time_taken);
     printf("\n%d\n",marks);
 	if(marks==28){
 	printf("\npassed\n");
@@ -587,7 +596,63 @@ for(int k=0;k<4;k++){
 }
 
 int main(){
-	attempt_test();
-	
+ char test[4];
+ MYSQL *conn;
+ MYSQL_RES *res, *res1, *res2, *res3,*res4, *res5, *res6, *res7, *res8;
+ MYSQL_ROW row;
+
+ char *server = "localhost";
+ char *user = "root";
+ char *password = ""; /*password is not set in this example*/
+ char *database = "kindacare";
+
+ conn = mysql_init(NULL);
+
+ /* Connect to database */
+ if (!mysql_real_connect(conn, server,
+ user, password, database, 0, NULL, 0))
+ {
+   printf("Failed to connect MySQL Server %s. Error: %s\n", server, mysql_error(conn));
+   return 0;
+ }
+
+ /* send SQL query */
+ if (mysql_query(conn, "select Q1 from assignment where AssignmentID = 2"))
+ {
+   printf("Failed to execute query. Error: %s\n", mysql_error(conn));
+   return 0;
+ }
+
+
+ res = mysql_store_result(conn);
+ if (res == NULL)
+ {
+   return 0;
+ }
+
+ int columns = mysql_num_fields(res);
+
+ int i = 0;
+
+ printf("Entries in the table assignment:\n");
+ while(row = mysql_fetch_row(res))
+ {
+   for (i = 0; i < columns; i++)
+   {
+     printf("%s ", row[i] ? row[i] : "NULL");
+     /*store test in string*/
+     strcpy(test,row[i]);
+
+   }
+   printf("\n");
+ }
+
+
+ mysql_free_result(res);
+ mysql_close(conn);
+
+
+	attempt_test(test);
+
 }
 
